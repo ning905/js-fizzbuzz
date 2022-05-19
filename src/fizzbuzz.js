d8f4626d0b8da069eb1140b7ad618d1a7dfbdae4
Ning Ma <ningma905@gmail.com>
2022-05-19 17:18:31 +0100

    complete fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,3 +2,25 @@
 
 // TODO: Change undefined below to the name of your function
 module.exports = undefined
+
+const fizzBuzzArray = []
+
+function fizzBuzz(lower, upper) {
+    for (let i = lower; i <= upper; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            fizzBuzzArray.push('FizzBuzz')
+        } else if (i % 3 === 0) {
+            fizzBuzzArray.push('Fizz')
+        } else if (i % 5 === 0) {
+            fizzBuzzArray.push('Buzz')
+        } else {
+            fizzBuzzArray.push(i)
+        }
+    }
+    return fizzBuzzArray
+}
+
+const lower = 1
+const upper = 15
+
+console.log(fizzBuzz(lower, upper))
